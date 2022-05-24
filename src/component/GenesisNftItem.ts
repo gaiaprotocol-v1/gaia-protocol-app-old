a148e9f880a0c9d3630dd596a6088f5e3758d967
dilrong <dilrong@dilrong.com>
2022-05-24 21:56:07 +0900

    styles: claim...
    connectWallet

--- src/component/GenesisNftItem.ts
+++ src/component/GenesisNftItem.ts
@@ -38,21 +38,14 @@ export default class GenesisNftItem extends DomNode {
                         this.klayDisplay = el(".klay", "... KLAY"),
                     ),
                 ),
-                // el(".button-wrap",
-                //     el("button.klay-button", msg("CLAIM_KLAY_BUTTON"), {
-                //         click: () => {
-                //             new Prompt(msg("CLAIM_KLAY_ALERT_TITLE"), msg("CLAIM_KLAY_ALERT_DESC"), msg("CLAIM_ALERT_BUTTON"), async (amount) => {
-                //                 const klay = utils.parseEther(amount);
-                //                 if (klay > this.klay) {
-                //                     new Alert(msg("CLAIM_ERROR_ALERT_TITLE"), msg("CLAIM_ERROR_ALERT_DESC"))
-                //                 } else {
-                //                     await GaiaOperationContract.claimKlayViaZap([this.id], [this.krno.mul(klay).div(this.klay)], klay, []);
-                //                     ViewUtil.waitTransactionAndRefresh();
-                //                 }
-                //             }, msg("CLAIM_PLACEHOLDER_INPUT"));
-                //         },
-                //     }),
-                // ),
+                el(".button-wrap",
+                    this.claimButton = el("a.klay-button", "미수령 이자 받기", {
+                        click: async () => {
+                            await GaiaGenesisUSDCDistributorContract.claim([this.id]);
+                            ViewUtil.waitTransactionAndRefresh();
+                        }
+                    }),
+                ),
             ),
             el(".content-wrap",
                 el("section",
@@ -62,13 +55,7 @@ export default class GenesisNftItem extends DomNode {
                     ),
                 ),
                 el(".button-wrap",
-                    this.claimButton = el("button.klay-button", "미수령 이자 받기", {
-                        click: async () => {
-                            await GaiaGenesisUSDCDistributorContract.claim([this.id]);
-                            ViewUtil.waitTransactionAndRefresh();
-                        }
-                    }),
-                    el("button.klay-button", msg("CLAIM_REWARDS_BUTTON"), {
+                    el("a.klay-button", msg("CLAIM_REWARDS_BUTTON"), {
                         click: async () => {
                             await NFTAirdropContract.collectAirdropReward(0, [this.id]);
                             ViewUtil.waitTransactionAndRefresh();
