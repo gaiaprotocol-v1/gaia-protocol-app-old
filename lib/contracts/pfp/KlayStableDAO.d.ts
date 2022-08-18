e65b69819cb3ed2f3c3627076480f6c2e0d3367b
Sim Young Jae <hanul@hanul.me>
2022-08-18 15:14:41 +0900

    dev

---
+++ lib/contracts/pfp/KlayStableDAO.d.ts
@@ -0,0 +1,1 @@
+//# sourceMappingURL=KlayStableDAO.d.ts.map
---
+++ lib/ethereum/EthereumNetworkProvider.d.ts
@@ -0,0 +1,14 @@
+import { ethers } from "ethers";
+import { EventContainer } from "skydapp-common";
+declare class EthereumNetworkProvider extends EventContainer {
+    private ethereum;
+    private get existsInjectedProvider();
+    provider: ethers.providers.JsonRpcProvider;
+    signer: ethers.providers.JsonRpcSigner;
+    constructor();
+    getBlockNumber(): Promise<number>;
+    getBalance(address: string): Promise<ethers.BigNumber>;
+}
+declare const _default: EthereumNetworkProvider;
+export default _default;
+//# sourceMappingURL=EthereumNetworkProvider.d.ts.map
